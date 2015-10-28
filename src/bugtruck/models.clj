0faf4f765944e28387401b22c6f85a46801bbd6c
minkitmih <minkitmih@gmail.com>
2015-10-27 23:16:35 -0700

    finished models design

--- src/bugtruck/models.clj
+++ src/bugtruck/models.clj
@@ -33,3 +33,55 @@
 
 (defn project-by-id [id]
   (first (select project (where {:id id}))))
+
+(defn all-issues []
+  (select issue))
+
+(defn issue-query [criteria]
+  ;; TODO: use partial instead
+  (select issue
+          (join status (= :status :status.id))
+          (where criteria)))
+
+(defn issues-by-project [id]
+  (-> {:project_id id}
+      issue-query
+      first))
+
+(defn issue-by-id [id]
+  (-> {:issue.id id}
+      issue-query
+      first))
+
+(defn comments-by-issue [id]
+  (select comment
+          (where {:issue_id id})
+          (order :id)))
+
+(defn status-by-name [s]
+  (first (select status (where {:name s}))))
+
+(defn delete-project [id]
+  (delete project (where {:id id})))
+
+(defn update-project [id params]
+  (update project
+          (set-fields params)
+          (where {:id id})))
+
+(defn create-issue [params]
+  (insert issue (values (select-keys params [:project_id :description :status]))))
+
+(defn create-comment [params]
+  (insert comment (values (select-keys params [:issue_id :content]))))
+
+(defn close-issue [id sid]
+  (update issue
+          (set-fields {:status sid})
+          (where {:id id})))
+
+;(defn find-issues [q]
+;  (let [q (str "%" (string/lower-case q) "%")]
+;    (-> (or (like (sqlfn lower :issue.title) q)
+;                   (like (sqlfn lower :issue.description) q))
+;        issue-query)))
